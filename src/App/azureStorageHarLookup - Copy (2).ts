8e4197367677db413a2531a33e31f3a81e102737
Bestguy900 <VitaliiBusiness990@gmail.com>
2021-04-08 09:49:50 +0400

    Version

---
+++ src/App/azureStorageHarLookup - Copy (2).ts
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.// Licensed under the MIT license.
+import * as fs from 'fs';
+import harServerUrl from "./harServerUrl.js"
+import Config from "./config.js";
+import Logger from "./logger.js";
+var azure = require('azure-storage');
+
+export default class AzureStorageHarLookup {
+  HarFiles: string[] = []; 
+
+  private azureStorageAccountName: string = Config.Instance().AzureStorageAccountName;
+  private azureStorageAccessKey: string = Config.Instance().AzureStorageAccessKey;
+  private azureStorageContainerName: string = Config.Instance().AzureStorageContainerName;
+  private blobService;
+
+  constructor(azureOverride=undefined) {
+    this.initialize(azureOverride);
+  }
+  
+  private static _instance: AzureStorageHarLookup = null;
+
+  public static Instance(): AzureStorageHarLookup {
+    if (AzureStorageHarLookup._instance === null) {
+      AzureStorageHarLookup._instance = new AzureStorageHarLookup();
+    }
+
+    return AzureStorageHarLookup._instance;
+  }
+
+  protected initialize(azureOverride) {
+    if (this.azureStorageAccountName.length > 0) {
+      this.blobService = azure.createBlobService(this.azureStorageAccountName, this.azureStorageAccessKey);
+    }
+    else if (azureOverride) {
+      this.blobService = azureOverride.createBlobService();
+    }
+  }
+
+  public LoadFile(filename: string, onDownloaded) {
+    this.blobService.getBlobToText(this.azureStorageContainerName, filename, (function (err, blobContent, blob) {
+      if (!err) {
+        onDownloaded(blobContent);
+      }
+      else {
+        Logger.Instance().Log('error', 'Error downloading har file from Azure storage: ' + err.message);
+        onDownloaded(null);
+      }
+    }).bind(this));
+  }
+}
